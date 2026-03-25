7aa1e301f65f2bb60c74a852971896d0f418b36c
harry246254 <113370074+harry246254@users.noreply.github.com>
2026-03-25 11:56:07 +0800

    Update main.c

--- HW02/HW02_A111193/HW02_A111193/user/main.c
+++ HW02/HW02_A111193/HW02_A111193/user/main.c
@@ -1,33 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<string.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h> 
 
 int main() 
 {
-    char *to_driver ="Prepare to write"; 
+    const char *to_driver = "Prepare to write"; 
     int size = strlen(to_driver);
-    char to_user[size];
+    
+    char to_user[size + 1];
+    memset(to_user, 0, sizeof(to_user));
     
     int fd = open("/dev/demo", O_RDWR);
-    ssize_t ret;
-    if (fd == -1)
-    {
-        printf("Can not open file\n");
+    if (fd == -1) {
+        printf("Can not open file");
         return -1;
     }
-    ret = write(fd, to_driver, size);
-    if (ret > 0)
+
+    ssize_t ret = write(fd, to_driver, size);
+    if (ret == -1) {
         printf("Write fail\n");
-
+    } 
+    
     ret = read(fd, &to_user[0], size);
-    if (ret > 0)
+    if (ret == -1) {
         printf("Read fail\n");
-    printf("%s\n", to_user);
-    close(fd);
+    } else {
+        to_user[ret] = '\0';
+        printf("%s\n", to_user);
+    }
 
+    close(fd);
     return 0;
 }
-
